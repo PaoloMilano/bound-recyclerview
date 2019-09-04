4c4bbb51e3f1268f4723835543bda201752aeda9
Paolo Milano <spacitron999@gmail.com>
2019-09-05 00:02:32 +0200

    Added dummy implementation for testing

--- app/src/main/java/com/magicbluepenguin/boundrecyclerview/DifferentiableObject.kt
+++ app/src/main/java/com/magicbluepenguin/boundrecyclerview/DifferentiableObject.kt
@@ -4,3 +4,8 @@ interface DifferentiableObject {
     fun hasSameId(other: DifferentiableObject): Boolean
     fun hasSameContents(other: DifferentiableObject): Boolean
 }
+
+class DummyDifferentiableObject: DifferentiableObject{
+    override fun hasSameContents(other: DifferentiableObject) = false
+    override fun hasSameId(other: DifferentiableObject) = false
+}
